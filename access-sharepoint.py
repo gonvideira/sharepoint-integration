48d5440eb606641cae9499c449698d9e553c4b6f
Gonçalo <138708856+gonvideira@users.noreply.github.com>
2023-07-15 07:55:45 +0100

    Update access-sharepoint.py

--- access-sharepoint.py
+++ access-sharepoint.py
@@ -1,9 +1,18 @@
+import os
 from office365.runtime.auth.user_credential import UserCredential
 from office365.sharepoint.client_context import ClientContext
 
-site_url = "https://{your-tenant-prefix}.sharepoint.com"
-ctx = ClientContext(site_url).with_credentials(UserCredential("{username}", "{password}"))
-web = ctx.web
-ctx.load(web)
-ctx.execute_query()
-print("Web title: {0}".format(web.properties['Title']))
+TENANT_NAME = os.environ['secrets.TENANT_NAME']
+USER_NAME = os.environ['secrets.USER_NAME']
+PASSWORD = os.environ['secrets.PASSWORD']
+
+def access():
+  site_url = f"https://{TENANT_NAME}.sharepoint.com"
+  ctx = ClientContext(site_url).with_credentials(UserCredential(f"{USER_NAME}", f"{PASSWORD}"))
+  web = ctx.web
+  ctx.load(web)
+  ctx.execute_query()
+  print("Web title: {0}".format(web.properties['Title']))
+
+if __name__ == "__main__":
+  return access()
