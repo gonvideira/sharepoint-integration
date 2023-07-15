80777b55dccc4cc145432b58db12bbbb62294e21
Gonçalo <138708856+gonvideira@users.noreply.github.com>
2023-07-15 08:07:32 +0100

    corrected python code

--- access-sharepoint.py
+++ access-sharepoint.py
@@ -2,9 +2,9 @@ import os
 from office365.runtime.auth.user_credential import UserCredential
 from office365.sharepoint.client_context import ClientContext
 
-TENANT_NAME = os.environ['secrets.TENANT_NAME']
-USER_NAME = os.environ['secrets.USER_NAME']
-PASSWORD = os.environ['secrets.PASSWORD']
+TENANT_NAME = os.environ['TENANT_NAME']
+USER_NAME = os.environ['USER_NAME']
+PASSWORD = os.environ['PASSWORD']
 
 def access():
   site_url = f"https://{TENANT_NAME}.sharepoint.com"
