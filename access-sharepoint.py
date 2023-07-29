5e3145bbbd3903c92d913502eb1bc57adadd3b35
Gonçalo <138708856+gonvideira@users.noreply.github.com>
2023-07-29 14:48:53 +0100

    Update access-sharepoint.py

--- access-sharepoint.py
+++ access-sharepoint.py
@@ -16,10 +16,10 @@ def access():
   print("Web title: {0}\n".format(web.properties['Title']))
   return ctx
 
-def get_files(ctx_365):
+def get_files(client_context):
   """Function to get all files in folder"""
   lib_title = "OrderFilesSAP"
-  lib = ctx_365.web.lists.get_by_title(lib_title)
+  lib = client_context.web.lists.get_by_title(lib_title)
   recent_items = lib.items.order_by("Created desc").select(["ID", "FileRef"]).get().execute_query()
   for item in recent_items:  # type: ListItem
     file_url = item.properties.get("FileRef")
@@ -27,19 +27,16 @@ def get_files(ctx_365):
     download_path = 'outputs/'
     print(f'File URL: {file_url}\n')
     print(f'File Name: {file_name}\n')
-    download_file(ctx_365, file_url)
-    #with open(download_path, "wb") as local_file:
-    #    item.file.download(local_file).execute_query()
-    #print("[Ok] file has been downloaded into: {0}".format(download_path))
-  return None
+    download_file(client_context, file_url)
+  return print('All files downloaded!')
 
-def download_file(ctx_365, file_url):
+def download_file(ctx, file_url):
   """Function that downloads file bu url"""
   file_name = os.path.basename(file_url)
   download_folder = 'outputs/'
   download_path = download_folder + file_name
   with open(download_path, "wb") as local_file:
-    file = ctx_365.web.get_file_by_server_relative_url(file_url).download(local_file).execute_query()
+    file = ctx.web.get_file_by_server_relative_url(file_url).download(local_file).execute_query()
   print("[Ok] file has been downloaded into: {0}".format(download_path))
 
 if __name__ == "__main__":
