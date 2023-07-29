3b67f6e7c117e8c587f43518105e0f5fe32a42a5
Gonçalo <138708856+gonvideira@users.noreply.github.com>
2023-07-29 13:14:43 +0100

    included list of files

--- access-sharepoint.py
+++ access-sharepoint.py
@@ -1,7 +1,6 @@
 import os
 from office365.runtime.auth.user_credential import UserCredential
 from office365.sharepoint.client_context import ClientContext
-# import tempfile
 
 TENANT_NAME = os.environ['TENANT_NAME']
 USER_NAME = os.environ['USER_NAME']
@@ -16,9 +15,23 @@ def access():
   print("Web title: {0}".format(web.properties['Title']))
   return ctx
 
+def get_files(ctx_365):
+  """Function to get all files in folder"""
+  ctx = ClientContext(test_site_url).with_credentials(test_user_credentials)
+  lib_title = "OrderFilesSAP"
+  lib = ctx.web.lists.get_by_title(lib_title)
+  recent_items = lib.items.order_by("Created desc").select(["ID", "FileRef"]).get().execute_query()
+  for item in recent_items:  # type: ListItem
+    file_url = item.properties.get("FileRef")
+    download_path = 'outputs/'
+    print(f'File: {file_url}\n')
+    #with open(download_path, "wb") as local_file:
+    #    item.file.download(local_file).execute_query()
+    #print("[Ok] file has been downloaded into: {0}".format(download_path))
+  return None
+
 def download_file(ctx_365):
   file_url = 'OrderFilesSAP/StoreOrder_A026_2023-07-29.csv'
-  # download_path = os.path.join(tempfile.mkdtemp(), os.path.basename(file_url))
   download_path = 'test.csv'
   with open(download_path, "wb") as local_file:
     file = ctx_365.web.get_file_by_server_relative_url(file_url).download(local_file).execute_query()
