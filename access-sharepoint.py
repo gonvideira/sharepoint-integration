79b40ed59debb38f804453e9ec58190f70379cec
Gonçalo <138708856+gonvideira@users.noreply.github.com>
2023-07-29 12:01:48 +0100

    Update access-sharepoint.py

--- access-sharepoint.py
+++ access-sharepoint.py
@@ -1,6 +1,7 @@
 import os
 from office365.runtime.auth.user_credential import UserCredential
 from office365.sharepoint.client_context import ClientContext
+import tempfile
 
 TENANT_NAME = os.environ['TENANT_NAME']
 USER_NAME = os.environ['USER_NAME']
@@ -13,6 +14,13 @@ def access():
   ctx.load(web)
   ctx.execute_query()
   print("Web title: {0}".format(web.properties['Title']))
+
+def download_file(ctx):
+  file_url = 'Shared Documents/big_buck_bunny.mp4'
+  download_path = os.path.join(tempfile.mkdtemp(), os.path.basename(file_url))
+  with open(download_path, "wb") as local_file:
+    file = ctx.web.get_file_by_server_relative_url(file_url).download(local_file).execute_query()
+  print("[Ok] file has been downloaded into: {0}".format(download_path))
 
 if __name__ == "__main__":
   access()
